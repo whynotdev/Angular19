97cb8424f4794ef9882467ba9e5d978809e92875
whynotdev <whynotmee52@gmail.com>
2025-03-07 12:40:33 +0530

    serviceRoutingFormsMaterial

--- src/app/Directives/ng-for-directive/ng-for-directive.component.ts
+++ src/app/Directives/ng-for-directive/ng-for-directive.component.ts
@@ -1,11 +1,24 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for-directive',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './ng-for-directive.component.html',
-  styleUrl: './ng-for-directive.component.css'
+  styleUrl: './ng-for-directive.component.css',
 })
 export class NgForDirectiveComponent {
   items = ['Apple', 'Banana', 'Cherry', 'Date'];
+
+
+  employee: any[]= [{
+    empid:12,empName:"arif",
+  },
+  {
+    empid:12,empName:"arif",
+  },{
+    empid:12,empName:"arif",
+  },{
+    empid:12,empName:"arif",
+  }]
 }
